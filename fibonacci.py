13421bc860d45fc2273c33a0b6c5c9c895692d12
jazminMG <jaz_minmedina96@hotmail.com>
2016-08-22 12:49:36 -0500

    Modificacion

--- fibonacci.py
+++ fibonacci.py
@@ -4,5 +4,14 @@ def fibo_1(max):
 	while a<max:
 		print(a)
 		a,b=b,a+b
-
+def fibo_2(max):
+	serie = []
+	a, b = 0, 1
+	
+	while a < max:
+		serie.append (a)
+		a, b = b, a+b
+	return serie
+	
 fibo_1(10)		
+print (fibo_2(10))
